79dcf3d3ba633c6077639b66c96fa48218a6b471
Graham Goebel <grahamgoebel4@gmail.com>
2020-10-05 21:54:02 -0700

    last commit for now

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # codeQuiz
+<img>/Users/grahamgoebel/Desktop/Screen Shot 2020-10-05 at 9.47.45 PM.png</img>
+I gave this assingment my best effort with the use of my tutor and the best available resources. 
+I was unable to figure out how to change the screen to display the questions. 
+This is an assingment I will need to come back to because I need to work on the new assignment.
---
+++ assets/js/quiz.js
@@ -0,0 +1,35 @@
+const myQuestions = [ {
+
+title: "Is Javascript a front-end, back-end, or full-stack programming language?",
+choices: [ "front-end", "back-end","full-stack"
+],
+correctAnswer: "c"
+},
+{
+question: "What symbol is used to denote the start and end of a function?",
+answers: {
+    a: "parentheses",
+    b: "curly brackets",
+    c: "angle brackets"
+},
+correctAnswer: "b"
+},
+{
+question: "What symbol is used to denote the start and end of an array?",
+answers: {
+    a: "brackets",
+    b: "angle brackets",
+    c: "parentheses"
+},
+correctAnswer: "a"
+},
+{
+question: "What function is used to print output to the console?",
+answers: {
+    a: "preventDefault",
+    b: "console.log",
+    c: "print"
+},
+correctAnswer: "b"
+}
+];
